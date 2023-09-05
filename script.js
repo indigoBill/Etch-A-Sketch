3e4b70972ad863aecf9a211a769e3c70b5153e0d
indigoBill <117851497+indigoBill@users.noreply.github.com>
2023-09-04 22:07:51 -0400

    Clean up code for better readability

--- script.js
+++ script.js
@@ -21,14 +21,13 @@ function defaultGrid(){
     }
 }
 
-//GENERATES A DEFAULT 16X16 GRID EVERY TIME THE WINDOW IS LOADED/REFRESHED
 window.addEventListener('load', defaultGrid);
 
 function randomIntGenerator(maxInt){
     return Math.floor(Math.random() * maxInt);
 }
 
-//RGB COLORS HAVE A RANGE FROM 0 TO 255
+//RGB COLORS ONLY GO UP TO 255
 function randomRgbColorGenerator(){
     let r = randomIntGenerator(255);
     let g = randomIntGenerator(255);
@@ -50,17 +49,18 @@ function hoverRainbow(e){
 }
 
 function hoverDarken(e){
-    let prevColor = e.target.style.backgroundColor.slice(4).slice(0,-1); //SEPARATE NUMBERS FROM RGB()
-
-    let rgbArr = prevColor.split(','); //PLACE EACH VALUE IN AN ARRAY
-    let newRgbArr = []; //CREATE EMPTY ARRAY
+    let prevColor = e.target.style.backgroundColor.slice(4).slice(0,-1);
+    let rgbArr = prevColor.split(',');
+    let newColor = [];
+    let shadeFactor = .1;
 
     for(let i = 0; i < rgbArr.length; i++){
-        let newRgb = Math.floor(rgbArr[i] * (1 - .2)); //FORMULA FOR ADDING SHADE TO RGB VALUES IS newR = currentR * (1 - shade_factor) <- REPEAT FOR G & B
-        newRgbArr.push(newRgb);
+        //FORMULA FOR ADDING SHADE TO RGB VALUES IS newR = currentR * (1 - shade_factor) <- REPEAT FOR G & B
+        let newRgb = Math.floor(rgbArr[i] * (1 - shadeFactor)); 
+        newColor.push(newRgb);
     }
 
-    e.target.style.backgroundColor = `rgb(${newRgbArr})`;  
+    e.target.style.backgroundColor = `rgb(${newColor})`;  
 }
 
 function erase(e){
@@ -90,18 +90,29 @@ function colorChange(e){
         squareHover.forEach(square => square.removeEventListener('mouseover', hoverRainbow));
         squareHover.forEach(square => square.removeEventListener('mouseover', erase));
         squareHover.forEach(square => square.addEventListener('mouseover', hoverDarken));
-    }else{
+    }else if(text === 'ERASE'){
         squareHover.forEach(square => square.removeEventListener('mouseover', hoverBlack));
         squareHover.forEach(square => square.removeEventListener('mouseover', hoverRainbow));
         squareHover.forEach(square => square.removeEventListener('mouseover', hoverDarken));
         squareHover.forEach(square => square.addEventListener('mouseover', erase));
+    }else{
+        resetSketchpad(slider.value);
     }
+}
+
+function mouseoverButton(e){
+    e.target.style.backgroundColor = 'grey';
+}
+
+function mouseoutButton(e){
+    e.target.style.backgroundColor = 'rgb(233, 233, 233)';
 }
 
 const colorButtons = document.querySelectorAll('.color-button');
 
-colorButtons.forEach(colorButton => colorButton.addEventListener('click', (e) => colorChange(e)));
-
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', colorChange));
+colorButtons.forEach(colorButton => colorButton.addEventListener('mouseover', mouseoverButton));
+colorButtons.forEach(colorButton => colorButton.addEventListener('mouseout', mouseoutButton));
 
 function resetSketchpad(numPerSide){
     while(gridContainer.firstChild){
@@ -123,15 +134,17 @@ function resetSketchpad(numPerSide){
     }
 }
 
-
-
-
 sliderValue.textContent = `GRID: ${slider.value} x ${slider.value}`;
 
 slider.oninput = () => {
     sliderValue.textContent = `GRID: ${slider.value} x ${slider.value}`;
+}
+
+slider.onchange = () => {
     resetSketchpad(slider.value);
 }
+
+
 
 
 
